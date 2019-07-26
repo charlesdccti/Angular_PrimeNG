5b625a972f216d2993f666f86ddd369fa2766e4e
charlesdccti <charlesdccti@gmail.com>
2019-07-26 18:05:09 -0300

    Start PrimeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import {CalendarModule} from 'primeng/calendar';
+import {RatingModule} from 'primeng/rating';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +19,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    TableModule,
+    HttpClientModule,
+    InputTextModule,
+    DialogModule,
+    ButtonModule,
+    CalendarModule,
+    RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
